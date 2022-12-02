281375961ee2899937718be7341ce18951906550
jmoffatt32 <jmoffatt32@gmail.com>
2022-12-02 16:39:04 -0500

    configuration changes to load images

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -3,7 +3,6 @@ import * as React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './src/styles/style.scss'
-import './src/styles/variables.scss'
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
--- frontend/webpack.config.development.js
+++ frontend/webpack.config.development.js
@@ -23,6 +23,10 @@ module.exports = {
                 ],
             },
             {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
                 test: /\.(js|jsx)$/, //kind of file extension this rule should look for and apply in test
                 exclude: /node_modules/, //folder to be excluded
                 use: 'babel-loader', //loader which we are going to use

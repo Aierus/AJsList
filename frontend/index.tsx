d422479b37cfb9f404bc67d2bb8f5eb840a71d00
jmoffatt32 <jmoffatt32@gmail.com>
2022-12-02 14:47:23 -0500

    convert App.js to App.tsx

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,5 +1,7 @@
-import * as React from "react";
-import * as reactDom from "react-dom";
-import App from "./src/App";
+import App from './src/App'
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
 
-reactDom.render(<App />, document.getElementById("root"));
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<App />)
---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+function App() {
+  return <h1>Welcome to AJ's list.</h1>;
+}
+
+export default App;

1ed12a81d49434a46d398b8f1c4d49512c2a6981
jmoffatt32 <jmoffatt32@gmail.com>
2022-12-02 16:39:43 -0500

    container refactored to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import { ThemeProvider } from '@mui/material/styles'
+import Container from '@mui/material/Container'
 
 import { theme } from './theme'
 import ProductsPage from './pages/Products'
@@ -9,10 +10,18 @@ import ProductsPage from './pages/Products'
 function App() {
     return (
         <ThemeProvider theme={theme}>
-            <Routes>
-                <Route path="" element={<ProductsPage />} />
-                <Route path="/:id" element={<h1>Product ID</h1>} />
-            </Routes>
+            <Container
+                maxWidth={false}
+                sx={{
+                    background: theme.palette.background.default,
+                    minHeight: '100vw',
+                }}
+            >
+                <Routes>
+                    <Route path="" element={<ProductsPage />} />
+                    <Route path="/:id" element={<h1>Product ID</h1>} />
+                </Routes>
+            </Container>
         </ThemeProvider>
     )
 }
